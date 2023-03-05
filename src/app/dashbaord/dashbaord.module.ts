c10296efcd7389615b843515833fa8a4d0d71b34
AliAbuRub <90558669+AliAbuRub@users.noreply.github.com>
2023-03-05 03:18:06 +0300

    adding all mudule files and routes

---
+++ src/app/dashbaord/dashbaord.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashbaordRoutingModule } from './dashbaord-routing.module';
+import { UserManagementComponent } from './components/user-management/user-management.component';
+import { BookManagementComponent } from './components/book-management/book-management.component';
+import { GroupManagementComponent } from './components/group-management/group-management.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+
+@NgModule({
+  declarations: [
+    UserManagementComponent,
+    BookManagementComponent,
+    GroupManagementComponent,
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    DashbaordRoutingModule
+  ]
+})
+export class DashbaordModule { }
